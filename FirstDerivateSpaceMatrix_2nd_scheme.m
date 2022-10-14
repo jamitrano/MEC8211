253a6281aaa13963cc182353b954a8498a7b1899
pgrosfils <paul.grosfils@polymtl.ca>
2022-10-14 19:30:43 -0400

    Création du schéma de 1e dérivation de 2e ordre

---
+++ FirstDerivateSpaceMatrix_2nd_scheme.m
@@ -0,0 +1,11 @@
+function [dfdx] = FirstDerivateSpaceMatrix(n,dx)
+%% INPUT 
+% n          | size of state vector (N,1) describing the system 
+% dx          | Spatial step for discretization 
+
+%% OUPUT 
+% dfdx       | Derivation Matrix : df/dx = dfdx*f  
+e = ones(n,1);
+z=zeros (n,1);
+dfdx = 1/(2*dx) *spdiags([-e z e],-1:1,n,n);
+end
